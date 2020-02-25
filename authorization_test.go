fd99d7664b6e115e270274c7a9151ad967f6cea4
Sterligov Denis <Sterligov Denis>
2020-02-25 10:28:20 +0300

    fix typo in word

--- authorization_test.go
+++ authorization_test.go
@@ -6,52 +6,52 @@ import (
 )
 
 func TestNoAuth(t *testing.T) {
-	b := []byte{2,0,1}
+	b := []byte{2, 0, 1}
 	buf := bytes.NewBuffer(b)
 	err := authorize(nil, buf)
 	if err != nil {
-		t.Errorf("Excpected nil error, got [%s]", err.Error())
+		t.Errorf("Expected nil error, got [%s]", err.Error())
 	}
 	expected := []byte{5, 0}
 	if !bytes.Equal(expected, buf.Bytes()) {
-		t.Errorf("Excpected %v, got %v", expected, buf.Bytes())
+		t.Errorf("Expected %v, got %v", expected, buf.Bytes())
 	}
 }
 
 func TestNoAcceptableAuthMethods(t *testing.T) {
-	b := []byte{1,3}
+	b := []byte{1, 3}
 	buf := bytes.NewBuffer(b)
 	err := authorize(nil, buf)
 	if err == nil {
-		t.Errorf("Excpected not acceptable error, got nil")
+		t.Errorf("Expected not acceptable error, got nil")
 	}
 	expected := []byte{5, 255}
 	if !bytes.Equal(expected, buf.Bytes()) {
-		t.Errorf("Excpected %v, got %v", expected, buf.Bytes())
+		t.Errorf("Expected %v, got %v", expected, buf.Bytes())
 	}
 }
 
 func TestBadLogin(t *testing.T) {
-	b := []byte{2,0,2,5,4,'u','s','e','r',3,'b','a','d'}
+	b := []byte{2, 0, 2, 5, 4, 'u', 's', 'e', 'r', 3, 'b', 'a', 'd'}
 	buf := bytes.NewBuffer(b)
 	err := authorize(&RoughAuthorizer{"user", "pass"}, buf)
 	if err == nil {
 		t.Errorf("Excpected bad login details error, got nil")
 	}
-	expected := []byte{5,2,1,1}
+	expected := []byte{5, 2, 1, 1}
 	if !bytes.Equal(expected, buf.Bytes()) {
 		t.Errorf("Excpected %v, got %v", expected, buf.Bytes())
 	}
 }
 
 func TestCorrectLogin(t *testing.T) {
-	b := []byte{2,0,2,5,4,'u','s','e','r',4,'p','a','s','s'}
+	b := []byte{2, 0, 2, 5, 4, 'u', 's', 'e', 'r', 4, 'p', 'a', 's', 's'}
 	buf := bytes.NewBuffer(b)
 	err := authorize(&RoughAuthorizer{"user", "pass"}, buf)
 	if err != nil {
 		t.Errorf("Excpected nil, got [%v]", err)
 	}
-	expected := []byte{5,2,1,0}
+	expected := []byte{5, 2, 1, 0}
 	if !bytes.Equal(expected, buf.Bytes()) {
 		t.Errorf("Excpected %v, got %v", expected, buf.Bytes())
 	}
